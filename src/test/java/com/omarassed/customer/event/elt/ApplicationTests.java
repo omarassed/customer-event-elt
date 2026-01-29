8a3ec8adf500f1633176d0614ea267ef0d27ca0b
Omar <omar.assed@gmail.com>
2026-01-29 15:28:12 -0700

    first commit

---
+++ src/test/java/com/omarassed/customer/event/elt/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omarassed.customer.event.elt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
